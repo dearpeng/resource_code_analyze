3026ae15954e4f8e5e15163b068c05a3e8be4fd2
晓梦初醒 <1249308431@qq.com>
2021-04-15 11:02:09 +0800

    第一次提交

--- src/main/java/com/luban/springsourcecodeanalysis/example/test.java
+++ src/main/java/com/luban/springsourcecodeanalysis/example/test.java
@@ -11,6 +11,11 @@ public class test {
         System.out.println("max memory:"+Runtime.getRuntime().maxMemory());
         System.out.println("free memory:"+Runtime.getRuntime().freeMemory());
         System.out.println("total memory:"+Runtime.getRuntime().totalMemory());
+        byte[] bytes = new byte[8*1024*1024];
+        System.out.println("请求8兆后max memory:"+Runtime.getRuntime().maxMemory());
+        System.out.println("请求8兆后free memory:"+Runtime.getRuntime().freeMemory());
+        System.out.println("请求8兆后total memory:"+Runtime.getRuntime().totalMemory());
+
 
     }
 }
